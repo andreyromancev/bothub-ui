27f3b6e9c0d5b102ddf5e132a8f0494176d55cfd
Andrey Romantsev <pixel33develop@gmail.com>
2017-05-20 03:00:27 +0300

    Добавил сторедж.

--- src/connect/bothub.ts
+++ src/connect/bothub.ts
@@ -1,54 +1,55 @@
-import axios, {
-    AxiosRequestConfig,
-    AxiosResponse,
-    AxiosError,
-    AxiosInstance,
-    AxiosAdapter,
-    AxiosPromise,
-    Cancel,
-    CancelToken,
-    CancelTokenSource,
-    Canceler,
-} from 'axios'
+import axios, {AxiosRequestConfig, AxiosPromise} from 'axios'
+
+import {Store} from '@/utils'
+import {HTTP_400_BAD_REQUEST} from '@/utils/http'
 
 
 export namespace Bothub {
     export function request(config: AxiosRequestConfig): AxiosPromise {
-        return axios_base.request(get_config(config))
+        return axios_base.request(get_config(config)).catch(handle_error)
     }
     export function get(url: string, config?: AxiosRequestConfig): AxiosPromise {
-        return axios_base.get(url, get_config(config))
+        return axios_base.get(url, get_config(config)).catch(handle_error)
     }
     export function del(url: string, config?: AxiosRequestConfig): AxiosPromise {
-        return axios_base.delete(url, get_config(config))
+        return axios_base.delete(url, get_config(config)).catch(handle_error)
     }
     export function head(url: string, config?: AxiosRequestConfig): AxiosPromise {
-        return axios_base.head(url, get_config(config))
+        return axios_base.head(url, get_config(config)).catch(handle_error)
     }
     export function post(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise {
-        return axios_post.post(url, get_data(data), get_config(config))
+        return axios_post.post(url, get_data(data), get_config(config)).catch(handle_error)
     }
     export function put(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise {
-        return axios_post.put(url, get_data(data), get_config(config))
+        return axios_post.put(url, get_data(data), get_config(config)).catch(handle_error)
     }
     export function patch(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise {
-        return axios_post.patch(url, get_data(data), get_config(config))
+        return axios_post.patch(url, get_data(data), get_config(config)).catch(handle_error)
     }
 
     export function authenticate(username: string, password: string) {
         const data = {'username': username, 'password': password}
         const config = {baseURL: API_ROOT}
-        const promise = axios_post.post(process.env.URL_API_AUTH, data, get_config(config))
+        return axios_post.post(process.env.URL_API_AUTH, data, get_config(config))
             .then((response) => {
                 auth_token = response.data.token
+                store.set(TOKEN_STORAGE_KEY, auth_token)
             })
-            .catch((error) => console.log(error.response.status))
+            .catch((error) => {
+                if (error.response.status === HTTP_400_BAD_REQUEST && auth_token) {
+                    auth_token = null
+                    store.del(TOKEN_STORAGE_KEY)
+                }
+            })
+    }
 
-        return promise
+    export function is_authenticated(): boolean {
+        return !!auth_token
     }
 
 
     const API_ROOT = process.env.URL_ROOT_API
+    const TOKEN_STORAGE_KEY = 'auth_token'
 
     const AXIOS_CONFIG_BASE = {
         baseURL: `${API_ROOT}/api/v1`,
@@ -60,14 +61,15 @@ export namespace Bothub {
 
     const axios_base = axios.create(AXIOS_CONFIG_BASE)
     const axios_post = axios.create(AXIOS_CONFIG_POST)
+    const store = new Store('bothub_api')
 
-    let auth_token: string
+    let auth_token: string = store.get(TOKEN_STORAGE_KEY)
 
     function get_config(config: AxiosRequestConfig = {}): AxiosRequestConfig {
         const result: AxiosRequestConfig = {headers: {}}
 
         if (auth_token) {
-            result.headers.Authorization = `JWT ${auth_token}`
+            result.headers.Authorization = `JWT ${auth_token}edw`
         }
 
         return Object.assign(result, config)
@@ -78,6 +80,8 @@ export namespace Bothub {
 
         return JSON.stringify(data)
     }
+
+    function handle_error(error) {
+        /* handle errors here */
+    }
 }
-
-
---
+++ src/utils/store.ts
@@ -0,0 +1,56 @@
+export class Store {
+    constructor(prefix: string, parent?: Store) {
+        this.parent = parent
+        this.prefix = prefix
+        if (parent) {
+            this.prefix = this.parent.prefix + Store.PATH_SEP + prefix
+        }
+    }
+
+    public set(key: string, data: any, update?: boolean) {
+        if (data === this.cache[key]) { return }
+
+        if (typeof data === 'object' && update) {
+            this.cache[key] = Object.assign(this.cache, data)
+        } else {
+            this.cache[key] = data
+        }
+
+        localStorage.setItem(this.prefix + Store.KEY_SEP + key, this.serialize(data))
+    }
+
+    public get(key: string): any {
+        if (this.cache[key] !== undefined) { return this.cache[key] }
+
+        const data = localStorage.getItem(this.prefix + Store.KEY_SEP + key)
+
+        return this.deserialize(data)
+    }
+
+    public del(key: string) {
+        delete this.cache[key]
+        localStorage.removeItem(this.prefix + Store.KEY_SEP + key)
+    }
+
+    private static PATH_SEP = '.'
+    private static KEY_SEP = ':'
+    private prefix: string = ''
+    private cache = {}
+    private parent: Store = null
+
+    private serialize(data: any): string {
+        if (typeof data === 'string') {
+            return data
+        }
+
+        return JSON.stringify(data)
+    }
+
+    private deserialize(data: string): any {
+        try {
+            return JSON.parse(data)
+        } catch (e) {
+            return data
+        }
+    }
+}
