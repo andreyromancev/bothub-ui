81d510d66cea617fa8114fde5b38c65f67116866
Andrey Romantsev <pixel33develop@gmail.com>
2017-05-22 01:16:56 +0300

    Убрал вывод конфига в консоль.

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,5 +2,3 @@ module.exports = {
   NODE_ENV: '"production"',
   URL_ROOT_API: JSON.stringify(process.env.URL_ROOT_API) || '"http://localhost:8000"',
 }
-
-console.log('TEST===============:' + JSON.stringify(process.env.URL_ROOT_API))
