406665ab722aa2874c0bfd47163ea11b93d4d603
Andrey Romantsev <pixel33develop@gmail.com>
2017-05-22 00:58:30 +0300

    Тестовый вывод для хероку.

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,3 +2,5 @@ module.exports = {
   NODE_ENV: '"production"',
   URL_ROOT_API: JSON.stringify(process.env.URL_ROOT_API) || '"http://localhost:8000"',
 }
+
+console.log('TEST===============:' + JSON.stringify(process.env.URL_ROOT_API))
